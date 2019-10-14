4b9af0336bd68f1fd8660b1f69c1b3595ce2a47c
ianmihuro <nickmihuro@gmail.com>
2019-10-14 10:41:18 +0300

    setting up our react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "mihuro-serverless-notes-uploads"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://v3w2lzsb4k.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_yHznkIbw7",
+        APP_CLIENT_ID: "6strpttbc7mmoucqjk142qc6l6",
+        IDENTITY_POOL_ID: "us-east-2:c6544fb4-47f4-44cb-8363-1c2d2092ffe8"
+    },
+};
